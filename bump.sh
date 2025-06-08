71e41b80a372390b0062c530ea297fab335d9f09
Max Plumley <maxplumley1@gmail.com>
2025-06-08 21:38:24 +1000

    fix: force semantic-release

--- bump.sh
+++ bump.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Check if both arguments are provided
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "Error: Invalid number of arguments"
-    echo "Usage: $0 <version>"
+    echo "Usage: $0 <version> <release_notes>"
     exit 1
 fi
 
-# Store the arguments
 VERSION="$1"
+RELEASE_NOTES="$2"
 
 echo "Bumping version to: $VERSION"
+echo "Release notes: $RELEASE_NOTES"
 
 echo "should_release=true" >> $GITHUB_OUTPUT
 echo "new_version=${VERSION}" >> $GITHUB_OUTPUT
+echo "release_notes=${RELEASE_NOTES}" >> $GITHUB_OUTPUT
 
 exit 0
